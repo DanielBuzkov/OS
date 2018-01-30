25fc9fce905164678f7fb2ff369e509ac0370cd1
ramel <ramel.kr@gmail.com>
2018-01-30 09:43:40 +0200

    irq

--- isr.c
+++ isr.c
@@ -5,8 +5,18 @@
 // This gets called from our ASM interrupt handler stub.
 void isr_handler(registers_t* regs)
 {
-   terminal_writestring("recieved interrupt: ");
-   char num[2] = {regs->int_no + '0', 0};
-   terminal_writestring(num);
-   terminal_writestring("\n");
+    terminal_writestring("recieved interrupt: ");
+    char num[2] = {regs->int_no + '0', 0};
+    terminal_writestring(num);
+    terminal_writestring("\n");
+    if(regs->int_no >= 40)
+    {
+        // Send reset signal to slave
+        outb(0xA0, 0x20);
+    }
+    if(regs->int_no >= 40)
+    {
+        // Send reset signal to master
+        outb(0x20, 0x20);
+    }
 }
