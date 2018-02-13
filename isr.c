c39d445a40f4834aa1fa1523e493b6d76ffda69b
ramel <ramel.kr@gmail.com>
2018-02-14 00:57:42 +0200

    array of interrupt handlers

--- isr.c
+++ isr.c
@@ -5,29 +5,50 @@
 // This gets called from our ASM interrupt handler stub.
 void isr_handler(registers_t* regs)
 {
-     if(regs->int_no >= 40)
-    {
-        // Send reset signal to slave
-        outb(0xA0, 0x20);
-    }
-    if(regs->int_no >= 32)
-    {
-        // Send reset signal to master
-        outb(0x20, 0x20);
-    }
+    // Send reset signal pic
+    send_reset(regs->int_no);
+
+    //ignore timer interupt
     if(regs->int_no == 32)
     {
         return;
     }
-    if(regs->int_no == 33)
+
+    //call specific interrupt handler
+    if (interrupt_handlers[regs->int_no] != 0)
     {
-        uint8_t c = inb(0x60);
-        char num[4] = {c/100 + '0',(c/10)%10+'0',c%10+'0', 0};
-        terminal_writestring(num);
-        terminal_writestring("\n");
+        interrupt_handlers[regs->int_no](regs);
     }
+    
     terminal_writestring("recieved interrupt: ");
     char num[3] = {regs->int_no/10 + '0',regs->int_no%10+'0', 0};
+    terminal_writestring(num);
+    terminal_writestring("\n");
+}
+
+void send_reset(uint32_t n)
+{
+     if(n >= 40)
+    {
+        // Send reset signal to slave
+        outb(0xA0, 0x20);
+    }
+     if(n >= 32)
+    {
+        // Send reset signal to master
+        outb(0x20, 0x20);        
+    }
+}
+
+void register_interrupt_handler(uint8_t n, isr_t handler)
+{
+    interrupt_handlers[n] = handler;
+}
+
+void keyboard_interrupt_handler(registers_t* regs)
+{
+    uint8_t c = inb(0x60);
+    char num[4] = {c/100 + '0',(c/10)%10+'0',c%10+'0', 0};
     terminal_writestring(num);
     terminal_writestring("\n");
 }
